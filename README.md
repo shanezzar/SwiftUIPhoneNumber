210f79db5c2449840236bc29ca54e8881c4a40f6
shanezzar <shanezzar@hotmail.com>
2021-08-08 12:51:41 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# SwiftUIPhoneNumber
+
+This is a pure SwiftUI implementation of phone number TextField, which is highly customizable.
+
+![alt text](preview.gif)
+
+## Minimal usage:
+```swift
+
+//
+@State var countryCode = ""
+@State var phoneNumber = UserModel.shared.phoneNumber
+
+// 
+SwiftUIPhoneNumber(countryCode: $countryCode, phoneNumber: $phoneNumber)
+
+```
+
+It takes country code (without +) and phone number. The code can be further customized by implementing extended functions like regex/validation etc. to the phone number.
+
+
+## Full implementation:
+```swift
+
+// 
+SwiftUIPhoneNumber(countryCode: <#T##Binding<String>#>, phoneNumber: <#T##Binding<String>#>, height: <#T##CGFloat#>, backgroundColor: <#T##Color#>, textColor: <#T##Color#>, textFont: <#T##Font#>, titleColor: <#T##Color#>, countryColor: <#T##Color#>, titleFont: <#T##Font#>, countryFont: <#T##Font#>)
+
+```
+
+### Some important notes:
+
+1. This library is just for iOS platfrom.
+2. Do not make height too small (passing height parameter). Find a suitable number and test it by giving various inputs(height).
+3. If in ZStack, make sure to set the outer view zIndex(1) or higher; e.g.,
+
+```swift
+
+//
+HStack {
+    SwiftUIPhoneNumber(countryCode: $countryCode, phoneNumber: $phoneNumber)
+}
+.frame(height: 60)
+.zIndex(1) // <- THIS
+
+```
+
+[@shanezzar](https://www.shanezzar.com).
---
+++ Sources/SwiftUIPhoneNumber/SwiftUIPhoneNumber.swift
@@ -0,0 +1,70 @@
+//
+//  SwiftUIPhoneNumber.swift
+//  
+//
+//  Created by Shanezzar on 06/08/2021.
+//
+//
+
+import SwiftUI
+
+@available(iOS 13.0, *)
+public struct SwiftUIPhoneNumber : View {
+    @Binding var countryCode: String
+    @Binding var phoneNumber: String
+    
+    let height: CGFloat
+    let backgroundColor: Color
+    let textColor: Color
+    let textFont: Font
+    let titleColor: Color
+    let countryColor: Color
+    let titleFont: Font
+    let countryFont: Font
+    
+    @State var countryFlag = ""
+    @State var y: CGFloat = 0
+    
+    public init(countryCode: Binding<String>, phoneNumber: Binding<String>, height: CGFloat = 600, backgroundColor: Color = Color.white, textColor: Color = Color.black, textFont: Font = Font.body, titleColor: Color = Color.black, countryColor: Color = Color.black, titleFont: Font = Font.title, countryFont: Font = Font.body) {
+        self._countryCode = countryCode
+        self._phoneNumber = phoneNumber
+        
+        self.height = height
+        self.backgroundColor = backgroundColor
+        self.textColor = textColor
+        self.textFont = textFont
+        self.titleColor = titleColor
+        self.countryColor = countryColor
+        self.titleFont = titleFont
+        self.countryFont = countryFont
+    }
+    
+    public var body: some View {
+        ZStack {
+            HStack (spacing: 0) {
+                Text("\(countryFlag) +\(countryCode)")
+                    .foregroundColor(textColor)
+                    .font(textFont)
+                    .onTapGesture {
+                        withAnimation (.spring()) {
+                            self.y = 0
+                        }
+                    }
+                
+                TextField("Phone Number", text: $phoneNumber)
+                    .keyboardType(.phonePad)
+                    .foregroundColor(textColor)
+                    .font(textFont)
+                    .padding()
+            }.padding()
+            
+            CountryCodes(countryCode: $countryCode, countryFlag: $countryFlag, y: $y, height: height, backgroundColor: backgroundColor, titleColor: titleColor, countryColor: countryColor, titleFont: titleFont, countryFont: countryFont)
+                .offset(y: y)
+        }
+        .onAppear {
+            countryFlag = "🇦🇺"
+            countryCode = "61"
+            self.y = height/2
+        }
+    }
+}
